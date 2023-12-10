6e18522c3510ccbdf7815ad95238e27ee2244159
aryankad1an <aryankaddian842@gmail.com>
2023-12-11 00:36:08 +0530

    initial commit 2

---
+++ module01.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is module 1!" << std::endl;
+    return 0;
+}
---
+++ module02.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is module 2!" << std::endl;
+    return 0;
+}
