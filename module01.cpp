efe44f1aad02aefb38958e509a79720102c4ff88
aryankad1an <aryankaddian842@gmail.com>
2023-12-11 17:35:18 +0530

    some updates

--- module01.cpp
+++ module01.cpp
@@ -7,6 +7,9 @@ int main() {
 
     // concept of data types helps the compiler to differentiate 4 characters(4 bytes) from 1 int(4 bytes too)
 
-
+    unsigned int a = -1;
+    cout << a << endl;
+    // here the value of a has been stores as negative(2's compliment) but when it is displayed it has not been converted back
+    // thus we get a really weird result(refer to screenshot for explanation)
     return 0;
 }
