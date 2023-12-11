ff5bc37e9993c09c9a59f60d6f802fa9a203921a
aryankad1an <aryankaddian842@gmail.com>
2023-12-11 17:42:27 +0530

    some updates

---
+++ module02.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Aryan Kadian on 11/12/23.
+//
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    /*
+     SOME GOOD PATTERNS
+     */
+
+    /*
+     PATTERN 1
+
+     1 2 3 4
+     1 2 3 4
+     1 2 3 4
+     1 2 3 4
+
+     */
+
+    return 0;
+}
