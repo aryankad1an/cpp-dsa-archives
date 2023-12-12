8b3c1da3ccd7b0f883fd7ea9f1e8acb649135053
aryankad1an <aryankaddian842@gmail.com>
2023-12-12 14:34:07 +0530

    module02 commit

--- module02.cpp
+++ module02.cpp
@@ -20,5 +20,52 @@ int main() {
 
      */
 
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= 4; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    /*
+     PATTERN 2
+     1
+     2 2
+     3 3 3
+     4 4 4 4
+     */
+
+    for (int i = 1; i <= 4; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    /*
+     PATTERN 3
+        1
+       121
+      12321
+     1234321
+     */
+
+    int seed = 4;
+    for (int i = 1; i <= seed; ++i) {
+        for (int j = seed-i; j > 0 ; --j) {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; ++j) {
+            cout << j;
+        }
+        for (int j = i-1; j >= 1 ; --j) {
+            cout << j;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
