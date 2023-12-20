b32a72edad2597623ed6c15a0105bc90b8ea0c60
aryankad1an <aryankaddian842@gmail.com>
2023-12-20 10:05:09 +0530

    functions/switch case

---
+++ module05.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+    // switch-case and functions
+    // refer to C++ core
+
+    // Function call Stack
+
+    /*
+     as the functions are called, they are stored in a stack like structure
+     the first function to be called is main function, so it will be the first to be inside a stack
+     the functions will be destroyed based on LIFO principle.
+
+     |Func3 |    <-- first to be out, last to be in
+     |Func2 |
+     |Func1 |
+     |Main  |
+     ________
+     */
+
+
+    // call by value: see core notes :)
+
+
+    return 0;
+}
