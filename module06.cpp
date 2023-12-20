ddbd2e445f107d052a7e72e07f8951e34108deae
aryankad1an <aryankaddian842@gmail.com>
2023-12-20 10:32:02 +0530

    reversing an array

--- module06.cpp
+++ module06.cpp
@@ -7,9 +7,27 @@ void printArray(int arr[], int size){
     }
 }
 
+void reverseArray(int arr[], int size){
+    // a good intuitive approach to this problem
+    int start = 0;
+    int end = size-1;
+    while(start <= end){
+        swap(arr[start], arr[end]); // built in swap function
+        start++;end--;
+    }
+}
 
 int main(){
     int nums[5] = {3, 4, 5, 6, 2};
     printArray(nums, size(nums));
+
+
+    // note that there is always call by reference for arrays
+    // it is because when we pass the arrays to function, we actualy pass the address of first element dawg
+
+
+    // reversing an array
+    reverseArray(nums, 5);
+    printArray(nums, 5);
     return 0;
 }
