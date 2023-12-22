e4b0a57a31ba8d62453c3ddd46a37720a8c1b9f3
aryankad1an <aryankaddian842@gmail.com>
2023-12-22 21:14:27 +0530

    unique element in array

--- module06.cpp
+++ module06.cpp
@@ -17,11 +17,22 @@ void reverseArray(int arr[], int size){
     }
 }
 
+int uniqueElementFromArray(int arr[], int size){
+    // some properties of xor which we can consider
+    // n^0=n (any number xor-ed to 0 is itself)
+    // n^n=0 (any number xor-ed to itself is zero)
+    int result = 0;
+    for (int i = 0; i < size; ++i) {
+        result^=arr[i];
+    }
+    return result;
+}
+
 int main(){
     int nums[5] = {3, 4, 5, 6, 2};
     printArray(nums, size(nums));
 
-
+    cout << "---" << endl;
     // note that there is always call by reference for arrays
     // it is because when we pass the arrays to function, we actualy pass the address of first element dawg
 
@@ -29,5 +40,11 @@ int main(){
     // reversing an array
     reverseArray(nums, 5);
     printArray(nums, 5);
+
+    cout << "---" << endl;
+
+    // finding unique in an array with duplicates in pairs
+    int findUnique[] = {4, 5, 5, 3, 4, 3, 69};
+    cout << uniqueElementFromArray(findUnique, 7) << endl;
     return 0;
 }
