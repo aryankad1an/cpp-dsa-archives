faf9f570ca51e339462232b48a4fdd46cd1a8694
aryankad1an <aryankaddian842@gmail.com>
2023-12-26 20:12:55 +0530

    vectors STL

--- module09.cpp
+++ module09.cpp
@@ -1,13 +1,45 @@
 #include<iostream>
 #include<array>
+#include<vector>
 
 using namespace std;
 int main(){
     // C++ STL(standard template library)
-    array<int, 4> a = {1,2,3,4}; //flashy way of writing arrays using STL
-    cout << a.front() << endl;//first element
-    cout << a.back() << endl; //last element
+    array<int, 4> a = {1,2,3,4}; // flashy way of writing arrays using STL
+    cout << a.front() << endl;// first element
+    cout << a.back() << endl; // last element
     cout <<a.empty() << endl; // tells if a is empty or not
 
+    vector<int> v; // dynamic arrays
+    cout << "Max Capacity ->" << v.capacity() << endl;
+    cout << "Size ->" << v.size() << endl;
+
+    v.push_back(1);
+    cout << "Max Capacity ->" << v.capacity() << endl;
+    cout << "Size ->" << v.size() << endl;
+
+    v.push_back(3);
+    cout << "Max Capacity ->" << v.capacity() << endl;
+    cout << "Size ->" << v.size() << endl;
+
+    v.push_back(2);
+    cout << "Max Capacity ->" << v.capacity() << endl;
+    cout << "Size ->" << v.size() << endl;
+
+    // note that vectors have dynamic size and "max capacities"
+    v.pop_back(); // removes last element
+    //v.clear(); // capacity remains same however size is 0'ed
+
+    vector<int> v2(5,1); // 5 elements, all initialised with 1
+    for (int i: v2) {
+        cout << i <<" ";
+    }
+    cout << endl;
+
+    vector<int> v3(v); // vector v3 initialised as copy of vector v
+    for (int i: v3) {
+        cout << i <<" ";
+    }
+    cout << endl;
     return 0;
 }
