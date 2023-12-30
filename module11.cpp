8d7d1fb4c78754265173c720e4fc073a909baaec
aryankad1an <aryankaddian842@gmail.com>
2023-12-30 16:14:18 +0530

    string questions

--- module11.cpp
+++ module11.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int main(){
+    char a = 'z';
+    // strings: 1 direction char arrays
+
+    string name = "aryan";
+
+    // legacy string operations(with char arrays): just like C(concept of null termination)
+    // refer to C language string notes ;)
 
     return 0;
 }
