8b361f32ffaa5f6e169e44d13e23c90db9d20a78
aryankad1an <aryankaddian842@gmail.com>
2024-01-01 23:18:31 +0530

    basic 2d array ques

--- module12.cpp
+++ module12.cpp
@@ -1,7 +1,59 @@
 #include<iostream>
 
 using namespace std;
-
 int main(){
+    // 2D arrays
+    // memory storage: refer to C notes
+
+    int row, col;
+    cin >> row >> col;
+    int arr[row][col];
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    // linear search of 2D Arrays: O(m*n) time complexity
+    {
+        int target;
+        cin >> target;
+        int found = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(arr[i][j] == target){
+                    cout << "row: " << i+1 << " col: " << j +1<< endl;
+                    found = 1;
+                }
+            }
+        }
+        if(!found){
+            cout << "Not found" << endl;
+        }
+    }
+
+    // row wise sum of 2D arrays
+    {
+        int sum = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                sum += arr[i][j];
+            }
+        }
+        cout << sum << endl;
+    }
+
+    // col wise sum of 2D arrays
+    {
+        int sum = 0;
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < row; j++){
+                sum += arr[j][i];
+            }
+        }
+        cout << sum << endl;
+    }
+
+
 
 }
